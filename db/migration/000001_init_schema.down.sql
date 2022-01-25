4e4a763d54cdbcd171f6ce88c8b3d46c9938d180
GabrielVitorGL <gabriel.lourenco18@etec.sp.gov.br>
2022-01-24 21:12:25 -0300

    aula 3

---
+++ db/migration/000001_init_schema.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS mudancas;
+DROP TABLE IF EXISTS transferencias;
+DROP TABLE IF EXISTS contas;
+/* usamos o drop em "mudancas" e "transferencias" primeiro pois elas usam chaves
+estrangeiras de "contas"*/
