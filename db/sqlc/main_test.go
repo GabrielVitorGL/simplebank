43bd813cdc6a7895662fbf467a7eec897a65ea6b
GabrielVitorGL <gabriel.lourenco18@etec.sp.gov.br>
2022-01-30 02:02:16 -0300

    aula 12

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,20 +7,19 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:pass412@localhost:5432/simple_bank?sslmode=disable"
+	"github.com/techschool/simplebank/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.CarregarConfig("../..")
+	if err != nil {
+		log.Fatal("nao foi possivel carregar as configuracoes:", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("nao foi possivel se conectar ao banco de dados:", err)
 	}
