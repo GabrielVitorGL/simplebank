79bb755225c155d01607fd46729ab69ac9b69e44
GabrielVitorGL <gabriel.lourenco18@etec.sp.gov.br>
2022-03-01 23:06:16 -0300

    descobrir de onde esta vindo o erro

--- start.sh
+++ start.sh
@@ -4,8 +4,11 @@ set -e
 
 echo "run db migration"
 source /app/app.env
+echo "source"
 DB_SOURCE_CORRIGIDO=`echo "$DB_SOURCE" | sed 's/[\]r//g'`
+echo "corrigiu"
 /app/migrate -path /app/migration -database "$DB_SOURCE_CORRIGIDO" -verbose up
+echo "rodou"
 
 echo "start the app"
 exec "$@"
