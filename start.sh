a12fd15fdb45e946974d0c75233a506dea4321e4
GabrielVitorGL <gabriel.lourenco18@etec.sp.gov.br>
2022-03-01 23:11:47 -0300

    testar outro start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,7 @@ set -e
 
 echo "run db migration"
 source /app/app.env
-echo "source"
-DB_SOURCE_CORRIGIDO=`echo "$DB_SOURCE" | sed 's/[\]r//g'`
-echo "corrigiu"
-/app/migrate -path /app/migration -database "$DB_SOURCE_CORRIGIDO" -verbose up
-echo "rodou"
+/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
 exec "$@"
