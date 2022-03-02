9ab14b5f47800f5a451fb55ad75fedcd5896a389
GabrielVitorGL <gabriel.lourenco18@etec.sp.gov.br>
2022-03-01 22:52:18 -0300

    corrigir erro do \r no final do URL do banco de dados
    O erro acontecia pois no final do URL do banco de dados sempre ficava com um \r, desta forma: "postgresql://root:pass412@localhost:5432/simple_bank?sslmode=disable\r"
    
    Então foi adicionado um código para retirar esse \r do final do URL

--- start.sh
+++ start.sh
@@ -4,7 +4,8 @@ set -e
 
 echo "run db migration"
 source /app/app.env
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+DB_SOURCE_CORRIGIDO=`echo "$DB_SOURCE" | sed 's/[\]r//g'`
+/app/migrate -path /app/migration -database "$DB_SOURCE_CORRIGIDO" -verbose up
 
 echo "start the app"
 exec "$@"
