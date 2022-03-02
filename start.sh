6db4a8b099dac2fcd1c9bcba797d765c7bcdcdd4
GabrielVitorGL <gabriel.lourenco18@etec.sp.gov.br>
2022-03-02 02:55:58 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ echo "ORIGINAL>>>"
 echo $DB_SOURCE
 echo "MODIFICADO>>>"
 echo $DB_SOURCE | sed 's/[\]r//g'
-/app/migrate -path /app/migration -database "postgresql://root:pass412@postgres:5432/simple_bank?sslmode=disable" -verbose up
+/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
 exec "$@"
